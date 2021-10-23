7b613980f200a6bedb896282d372d3203bd622f3
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-10-23 12:07:33 -0300

    Projecao dos dados do author com DTO

--- src/main/java/com/rafaelsouza/worshopmongo/config/Instantiation.java
+++ src/main/java/com/rafaelsouza/worshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.rafaelsouza.worshopmongo.domain.Post;
 import com.rafaelsouza.worshopmongo.domain.User;
+import com.rafaelsouza.worshopmongo.dto.AuthorDTO;
 import com.rafaelsouza.worshopmongo.repositories.PostRepository;
 import com.rafaelsouza.worshopmongo.repositories.UserRepository;
 
@@ -36,8 +37,8 @@ public class Instantiation implements CommandLineRunner{
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz dia", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz dia", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
