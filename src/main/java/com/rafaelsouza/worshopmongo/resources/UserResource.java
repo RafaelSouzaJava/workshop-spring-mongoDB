75a5dd3436997184fd29a75b192f2bcab12a33d8
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-10-23 12:23:31 -0300

    Endpoint para retorna os post de um usuario

--- src/main/java/com/rafaelsouza/worshopmongo/resources/UserResource.java
+++ src/main/java/com/rafaelsouza/worshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rafaelsouza.worshopmongo.domain.Post;
 import com.rafaelsouza.worshopmongo.domain.User;
 import com.rafaelsouza.worshopmongo.dto.UserDTO;
 import com.rafaelsouza.worshopmongo.services.UserService;
@@ -38,6 +39,12 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = userService.findById(id);	
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = userService.findById(id);			
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 	@PostMapping
